91d8731fd176680e6f5b903207db63a83656feb3
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-07-05 15:50:20 -0700

    create project repository

---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const NotFound = () => (
+    <h1>Nothing is found!</h1>
+);
+
+export default NotFound;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+import muiTheme from './config/theme';
+
+import Layout from './components/Layout';
+import Login from './containers/Login';
+import Items from './containers/Items';
+import NotFound from './containers/NotFound';
+
+injectTapEventPlugin();
+
+const Boomtown = () => (
+    <MuiThemeProvider muiTheme={muiTheme}>
+        <Layout>
+            <NotFound />
+        </Layout>
+    </MuiThemeProvider>
+
+);
+
+ReactDOM.render(<Boomtown />, document.getElementById('root'));
+registerServiceWorker();
