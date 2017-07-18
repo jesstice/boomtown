2cad54a2532e5196069b80218fa65817a9649925
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-07-17 18:44:13 -0700

    style 404

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import './styles.css';
 import Cat from '../../images/not-found.png';
 
 const NotFound = () => (
-    <div>
+    <div className="lost-container">
         <h1>I think you are lost...</h1>
         <img src={Cat} alt="lost cat" />
     </div>
